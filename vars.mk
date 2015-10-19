99c9a05fafdb2f387588af9fa46bf4257e801c0b
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2015-10-19 09:42:24 +0300

    Removed an unused variable

--- vars.mk
+++ vars.mk
@@ -2,8 +2,6 @@ GLIB_NAMESPACES = GLib-2.0 GObject-2.0 GModule-2.0 Gio-2.0
 GLIB_GIRFILES = $(addsuffix .gir,$(GLIB_NAMESPACES))
 GLIB_PACKAGES := $(shell echo $(GLIB_NAMESPACES) | tr A-Z a-z)
 
-submodules = src/glib src/gobject-introspection
-
 builddir = build
 glib_builddir = $(builddir)/glib
 gi_builddir = $(builddir)/gobject-introspection
